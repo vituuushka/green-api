e212115b767623f7cf4256fff63449a95a43c6f0
vituuushka <usmanova.vita13@gmail.com>
2025-02-05 01:19:26 +0400

    add: ghp

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react";
+import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../hooks/hooks";
 import { setChatId } from "../store/slices/chatSlice";
