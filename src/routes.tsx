6bcb73c247acfe612795232878b9d9e0f755d1a3
vituuushka <usmanova.vita13@gmail.com>
2025-02-05 13:46:33 +0400

    createHashRouter

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import ContactPage from "./pages/ContactPage";
 import App from "./App";
 import ChatPage from "./pages/ChatPage";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: "/", 
         element: <App />,
